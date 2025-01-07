3af7a2c86adcbb7832a396fb8f356a370372b666
Ilex00para <jacobstolz@web.de>
2025-01-07 13:55:32 +0000

    Handle double entries, using the error for control flow

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
         'raise_on_warnings': True
         }
     
+    'Remove as hardcode'
     API_KEY = "NNSXS.MLGVF5QOHIKWE6CM4CVTCINXIN5FYSYVBJWIRZQ.HGQBA3MLVVOX6XLWVQI26PSQX4TII5HT7NLP3YJZUIZTIDAEAMMA"
     
 
@@ -25,12 +26,8 @@ if __name__ == '__main__':
               
     api_connection = API_connection(API_KEY) #creates the connection to the API
     db_manager = DB_manager(config) #creates the manager (cursor) for interacting with the DB
-    climate_measurements = api_connection.get_data(minutes_back=5) #reads specific data (see mushroom_climate_control/packages/API_connection.py) 
-    print(climate_measurements)
-    """"Problem with the after parameter, not sending the most recent data"""
+    climate_measurements = api_connection.get_data(minutes_back=10) #reads specific data (see mushroom_climate_control/packages/API_connection.py) !!!!!!! Time is in UTC time!!!!!!! 
     t = api_connection.params['after'] #timestamp of the API call (when was it called)
-    data = db_manager.check_entry('climate_data', condition=f'measurement_time >= "{t}"') #checks if the data is already in the DB
-    print(data)
     """Can be improved by using multiple"""
     for measurement in climate_measurements:
         db_manager.writing_to_db(measurement, verbose=True) #writes for each climate timestamp the data to the db
